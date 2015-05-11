8f5a966bc514a545564af78761a992c837a2aaaa
Martin Bean <martin@martinbean.co.uk>
2015-05-11 10:15:19 +0100

    Add event tracking to book plugs

--- js/application.js
+++ js/application.js
@@ -4,4 +4,13 @@
     e.preventDefault();
     window.open(this.href);
   });
+
+  // Google Analytics event tracking
+  $('[data-track="event"]').on('click', function (e) {
+    if (_gaq) {
+      var category = $(this).data('category');
+      var label = $(this).data('label');
+      _gaq.push(['_trackEvent', category, 'Click', label]);
+    }
+  });
 })(jQuery);
