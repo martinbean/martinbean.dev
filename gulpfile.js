35586a5c7de931fa59879f867a3d5386a4f6dcc0
Martin Bean <martin@martinbean.co.uk>
2019-03-21 11:23:16 +0000

    Update Gulp version

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
-var gulp = require('gulp');
-var notify = require('gulp-notify');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const config = {
+  sass: {
+    outputStyle: 'compressed'
+  }
+};
 
-gulp.task('sass', function () {
-  gulp.src('./_sass/**/*.scss')
+gulp.task('sass', () => {
+  return gulp
+      .src('./_sass/**/*.scss')
       .pipe(sourcemaps.init())
-      .pipe(sass({
-        outputStyle: 'compressed'
-      }).on('error', sass.logError))
+      .pipe(sass(config.sass).on('error', sass.logError))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest('./css'))
       .pipe(notify('Sass compiled.'));
 });
 
-gulp.task('default', function () {
-  gulp.watch('./_sass/**/*.scss', ['sass']);
+gulp.task('build', gulp.series('sass'));
+
+gulp.task('watch', () => {
+  return gulp.watch('./_sass/**/*.scss', gulp.series('sass'));
 });
+
+gulp.task('default', gulp.series('build', 'watch'));
